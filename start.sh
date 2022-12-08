c60b01c84b6e5604df1233fc7c1ca5f33f3d0c8a
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2022-12-08 19:05:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/RoyalKrrishna/MdiskWalaBot.git /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/hintpirox/nikbot.git /nikbot
+cd /nikbot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
