d1d7778f915d2453b5a1e2b0126e88423e1358e1
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2022-12-10 16:21:28 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -174,32 +174,25 @@ class AsyncIter:
 
 # ################################################### Mdisk Convertor #########################################################
         
-# async def get_mdisk(link, api=Config.MDISK_API):
-async def get_mdisk(link, api=Config.MDISK_API):
-	url = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
-	param = {'token': api, 'link': link
-			 }
-	res = requests.post(url, json=param)
-	try:
-		shareLink = res.json()
-		link = shareLink["sharelink"]
-	except:
-		pass
-	return link
+async def get_mdisk(link):
+    url = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
+    param = {'token': MDISK_API, 'link': link
+             }
+    res = requests.post(url, json=param)
+    try:
+        shareLink = res.json()
+        link = shareLink["sharelink"]
+    except:
+        pass
+    return link
 
-async def replace_mdisk_link(text, api=Config.MDISK_API):
-    links = re.findall(r'https?://mdisk.me[^\s]+', text)
-    async for link in AsyncIter(links):
-        mdisk_link = await get_mdisk(link, api)
+
+async def replace_mdisk_link(text):
+    text = await replace_username(text)
+    text = await remove_emoji(text)
+    links = re.findall(r'https?://mdisk.me[^\s]+', str(text))
+    for link in links:
+        mdisk_link = await get_mdisk(link)
         text = text.replace(link, mdisk_link)
 
     return text
-
-
-async def group_link_convertor(group_id, text):
-    api = await db.get_api_id(group_id)
-    if api:
-        answer = await replace_mdisk_link(text, str(api['api']))
-    else:
-        answer = text
-    return answer
