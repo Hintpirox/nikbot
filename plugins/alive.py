8b78f4b694a64d1e30fddbe1aa7b158e48471e0e
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-01-08 14:14:20 +0530

    Create alive.py

---
+++ plugins/alive.py
@@ -0,0 +1,49 @@
+import asyncio
+import random
+from sys import version_info
+
+from pyrogram import __version__ as pver
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from telethon import __version__ as tver
+
+ASAU = [
+    [
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇꜱ", url=f"https://t.me/CyniteBackup"),
+        InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/CyniteSupport"),
+    ],
+    [
+        InlineKeyboardButton(
+            text="ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+            url=f"https://t.me/MdiskSearch_Robot?startgroup=true",
+        ),
+    ],
+]
+
+
+@Client.on_message(filters.command("alive"))
+async def restart(client, m: Message):
+    await m.delete()
+    accha = await m.reply("⚡")
+    await asyncio.sleep(1)
+    await accha.edit("ᴀʟɪᴠɪɴɢ..")
+    await asyncio.sleep(0.1)
+    await accha.edit("ᴀʟɪᴠɪɴɢ ʙᴀʙʏ ....")
+    await accha.delete()
+    await asyncio.sleep(0.1)
+    umm = await m.reply_sticker(
+        "CAACAgUAAx0CZIiVngABBHAzYwdi9OIVTQ7DYELAqMl46fgnK4wAAjsIAAKagolX-O0V64tvzK8pBA"
+    )
+    await asyncio.sleep(0.1)
+    await m.reply_photo(
+        random.choice(PHOTO),
+        caption=f"""**ʜᴇʏ, ɪ ᴀᴍ Mdisk Search Bot**
+     ▱▱▱▱▱▱▱▱▱▱▱▱
+» **ᴍʏ ᴏᴡɴᴇʀ :** [ᴄʟɪᴄᴋ ᴍᴇ](https://t.me/Cyniteofficial)
+» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tver}`
+» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pver}`
+» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{version_info[0]}.{version_info[1]}.{version_info[2]}`
+» **ʙᴏᴛ ᴠᴇʀꜱɪᴏɴ :** `1.0`
+     ▱▱▱▱▱▱▱▱▱▱▱▱""",
+        reply_markup=InlineKeyboardMarkup(ASAU),
+    )
